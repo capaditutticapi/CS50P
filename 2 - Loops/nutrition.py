1168d6ed76868a8b73bdecf071d60a46c1d8bee2
capaditutticapi <mobertsonx@gmail.com>
2025-11-13 11:55:56 +0000

    Create nutrition.py

---
+++ 2 - Loops/nutrition.py
@@ -0,0 +1,40 @@
+fruitinfo = {           
+    "apple":"130",
+    "avocado":"50",
+    "banana":"110",
+    "cantaloupe":"50",
+    "grapefruit":"60",
+    "grapes":"90",
+    "honeydew melon":"50",
+    "kiwifruit":"90",
+    "lemon":"15",
+    "lime":"20",
+    "nectarine":"60",
+    "orange":"80",
+    "peach":"60",
+    "pear":"100",
+    "pineapple":"50",
+    "plums":"70",
+    "strawberries":"50",
+    "sweet cherries":"100",
+    "tangerine":"50",
+    "watermelon":"80"
+}
+
+def main():
+    userinput = input("Item: ").lower()
+    calories = convert(userinput)
+    if calories == None:
+        return
+    else:
+        print("Calories:", calories)
+
+def convert(input):
+    for fruit in fruitinfo:
+        if fruit == input:
+            return fruitinfo[fruit]
+    else:
+        return None
+
+
+main()
