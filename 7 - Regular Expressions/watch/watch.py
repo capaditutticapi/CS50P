e843fa06143a11e7a502d5e485757ba026c8a84d
capaditutticapi <mobertsonx@gmail.com>
2025-12-02 23:47:59 +0000

    Create watch.py

---
+++ 7 - Regular Expressions/watch/watch.py
@@ -0,0 +1,15 @@
+import re
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    if match := re.search(r"<iframe src=\"https?://(?:www\.)?youtube\.com/embed/(.*?)\"></iframe>", s):
+        return f"https://youtu.be/{match.group(1)}"
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    main()
